518ea5f107f27343a38c721804c6d3a8b74f05aa
xingjian <xingjian@9a834573-2090-804c-9287-1a834bdb27e7>
2011-05-10 01:26:23 +0000

    Moved remotely
    git-svn-id: svn://10.10.191.1:3690/Algorithm/trunk@94 9a834573-2090-804c-9287-1a834bdb27e7

---
+++ Algorithm_web/src/com/xingjian/cn/algorithm/BubbleAlgorithm.java
@@ -0,0 +1,38 @@
+/**@文件名: BubbleAlgorithm.java @作者： promisePB xingjian@yeah.net @日期 2011-5-9 下午10:44:25 */
+
+package com.xingjian.cn.algorithm;
+
+/**   
+ * @类名: BubbleAlgorithm.java 
+ * @包名: com.xingjian.cn.algorithm 
+ * @描述: 冒泡法 
+ * @作者： promisePB xingjian@yeah.net   
+ * @日期： 2011-5-9 下午10:44:25 
+ * @版本： V1.0   
+ */
+
+public class BubbleAlgorithm {
+
+	/**
+	 * 功能：冒泡法
+	 * 描述：传入object,对object集合进行排序
+	 * @param object
+	 */
+	public Object bubble(Object object){
+		return null;
+	} 
+	
+	public int[] bubbleForInt(int[] array){
+		return null;
+	}
+	
+	/**
+	 * 功能：
+	 * 描述：
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
